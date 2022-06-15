ac48be812181a6c79fb59dfc8c2bb9228bd6e3f5
dzmac <dzmac@DESKTOP-RJ1J92J>
2022-06-15 22:07:28 +0200

    1

---
+++ UbraniaRozmiary/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace UbraniaRozmiary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(114, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(252, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Jakiego rodzaju ubrania szukasz?";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(114, 140);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            220,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(252, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wpisz swój wzrost (w cm):";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Zamknij";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 181);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(323, 59);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 345);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WYSZUKAJ ROZMIAR UBRANIA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ UbraniaRozmiary/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+namespace UbraniaRozmiary
+{
+    public partial class Form1 : Form
+    {
+
+
+        SQLiteConnection m_dbConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            textBox1.Visible = false;
+            zaladujRodzajeUbran();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if (System.IO.File.Exists("dane.sqlite") == false)
+            {
+                utworzBazeDanych();
+                polaczZBaza();
+                utworzTabele();
+
+            }
+
+            numericUpDown1.TextChanged += new EventHandler(numericUpDown1_TextChanged);
+
+
+        }
+
+
+
+
+
+
+
+
+        void utworzBazeDanych()
+        {
+            SQLiteConnection.CreateFile("dane.sqlite");
+        }
+
+
+        void polaczZBaza()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=dane.sqlite;Version=3;");
+            m_dbConnection.Open();
+        }
+
+ 
+        void utworzTabele()
+        {
+
+
+
+
+            string sql = "create table adiddas(id INTEGER  PRIMARY KEY, Rozmiar varchar(10),  Koszulki INTEGER, Bluzy INTEGER,  Spodnie INTEGER )";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "create table nike(id INTEGER  PRIMARY KEY, Rozmiar varchar(10),  Koszulki INTEGER, Bluzy INTEGER,  Spodnie INTEGER )";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+
+
+            sql = "insert into adiddas(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values (NULL, 'XS', '164', '164', '164')";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+           
+            sql = "insert into adiddas(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values (NULL, 'S', '170', '170', '170')";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into adiddas(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values (NULL, 'M', '176', '176', '176')";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+           
+            sql = "insert into adiddas(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values (NULL, 'L', '182', '182', '182')";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+           
+            sql = "insert into adiddas(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values (NULL, 'XL', '188', '188', '188')";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+          
+            sql = "insert into adiddas(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values (NULL, 'XXL', '192', '192', '192')";
+            command = new SQLiteCommand(sql,  m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into nike(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values (NULL, 'XS', '168', '168', '168')";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into nike(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values(NULL, 'S', 173, 173, 173)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            
+            sql = "insert into nike(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values(NULL, 'M', 178, 178, 178)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            
+            sql = "insert into nike(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values(NULL, 'L', 183, 183, 183)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            
+            sql = "insert into nike(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values(NULL, 'XL', 188, 188, 188)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            
+            sql = "insert into nike(id, Rozmiar, Koszulki, Bluzy, Spodnie ) values(NULL, 'XXL',  193, 193, 193)";
+            command = new SQLiteCommand(sql,  m_dbConnection);
+            command.ExecuteNonQuery();     
+            
+            
+            command.Dispose();
+            command = null;
+
+
+
+
+
+
+
+            m_dbConnection.Close();
+
+        }
+
+
+
+        private void zaladujRodzajeUbran()
+        {
+
+
+            comboBox1.Items.Add("Koszulki");
+            comboBox1.Items.Add("Bluzy");
+            comboBox1.Items.Add("Spodnie");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+           // wyszukaj();
+
+        }
+
+
+
+
+
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            wyszukaj();
+        }
+
+
+
+
+
+        void numericUpDown1_TextChanged(object sender, EventArgs e)
+        {
+            wyszukaj();
+        }
+
+
+
+
+
+
+
+
+
+        private void wyszukaj()
+        {
+
+
+            int wzrost = Convert.ToInt32(numericUpDown1.Value);
+
+            string wybrane_ubranie = "" + comboBox1.Text;
+            if (!wybrane_ubranie.Equals(""))
+            {
+
+
+                polaczZBaza();
+
+
+                string query1 = "";
+                string query2 = "";
+
+                string rozmiar1 = "";
+                string rozmiar2 = "";
+
+                query1 = "SELECT  Rozmiar from adiddas where " + wybrane_ubranie + "='" + wzrost + "' ";
+                query2 = "SELECT  Rozmiar from nike where " + wybrane_ubranie + "='" + wzrost + "' ";
+
+
+                SQLiteCommand command1 = new SQLiteCommand(query1, m_dbConnection);
+                SQLiteDataReader reader1 = command1.ExecuteReader();
+
+                int lp = 1;
+
+
+
+                while (reader1.Read())
+                {
+
+                    rozmiar1 = "" + reader1["Rozmiar"];
+
+
+                }
+                reader1.Close();
+                reader1.Dispose();
+                reader1 = null;
+
+
+
+                command1 = new SQLiteCommand(query2, m_dbConnection);
+                reader1 = command1.ExecuteReader();
+
+
+
+
+
+                while (reader1.Read())
+                {
+
+                    rozmiar2 = "" + reader1["Rozmiar"];
+
+
+                }
+                reader1.Close();
+                reader1.Dispose();
+                reader1 = null;
+
+
+
+                command1.Dispose();
+                command1 = null;
+
+
+
+                m_dbConnection.Close();
+                m_dbConnection.Dispose();
+                m_dbConnection = null;
+
+                string wynik = "";
+
+                if (!rozmiar2.Equals(""))
+                {
+                    wynik = "Nike: " + rozmiar2 + Environment.NewLine;
+
+                }
+                if (!rozmiar1.Equals(""))
+                {
+                    wynik += "Adidas: " + rozmiar1 + Environment.NewLine;
+
+                }
+
+                if (!wynik.Equals(""))
+                {
+
+                    wynik = "Dla Twojego wzrostu odpowiednie będzie ubranie o rozmiarze:" + Environment.NewLine + wynik; ;
+
+
+                    MessageBox.Show(wynik);
+                    textBox1.Visible = true;
+                }
+                else
+                {
+                    textBox1.Visible = false;
+
+
+                }
+
+
+                textBox1.Text = wynik;
+
+
+            }
+            else
+            {
+                textBox1.Visible = false;
+                MessageBox.Show("Wybierz rodzaj ubrania!");
+
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
